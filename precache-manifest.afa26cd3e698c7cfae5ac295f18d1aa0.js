3dd4152a0ca557cba3872d26cc47c877ebf684da
nabil999 <nabil.farouk@gmail.com>
2019-12-14 00:08:33 +0200

    Updates

---
+++ precache-manifest.afa26cd3e698c7cfae5ac295f18d1aa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c5980ad8ccfb0299e570db49a60f4d",
+    "url": "/deployrobots/index.html"
+  },
+  {
+    "revision": "181aa23dc467e8bf51c2",
+    "url": "/deployrobots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4f528f0ded24c002e891",
+    "url": "/deployrobots/static/css/main.e8125c3e.chunk.css"
+  },
+  {
+    "revision": "181aa23dc467e8bf51c2",
+    "url": "/deployrobots/static/js/2.2aa5c9ea.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/deployrobots/static/js/2.2aa5c9ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f528f0ded24c002e891",
+    "url": "/deployrobots/static/js/main.2fe048ed.chunk.js"
+  },
+  {
+    "revision": "f5eb42ef2eeb7e5342a2",
+    "url": "/deployrobots/static/js/runtime-main.1bad593c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/deployrobots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
